7de52f331b42851ff10921b74099e41f92253615
abrar0333 <abrar.h@briclay.com>
2025-09-10 08:32:03 +0530

    test: add single-number tests (red)

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -6,4 +6,10 @@ void main() {
     final calc = StringCalculator();
     expect(calc.add(''), equals(0));
   });
+
+  test('single number returns itself', () {
+    final calc = StringCalculator();
+    expect(calc.add('1'), equals(1));
+    expect(calc.add('42'), equals(42));
+  });
 }
