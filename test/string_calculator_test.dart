9b07b89928b49baa910337af6dcad12f0004ad1a
abrar0333 <abrar.h@briclay.com>
2025-09-10 08:21:35 +0530

    test: add empty-string test (red)

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,7 +2,8 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('empty string returns 0', () {
+    final calc = StringCalculator();
+    expect(calc.add(''), equals(0));
   });
 }
