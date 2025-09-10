1cc48f65984859fa92b4bb258a9507ed2883f503
abrar0333 <abrar.h@briclay.com>
2025-09-10 08:33:55 +0530

    feat: support single number (green)

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,10 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+
+    final single = int.tryParse(numbers.trim());
+    if (single != null) return single;
+
     return 0;
   }
 }
