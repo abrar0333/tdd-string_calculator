91fd03bafa4f4dba3051cb2ef980fcc91f77cebe
abrar0333 <abrar.h@briclay.com>
2025-09-10 08:37:32 +0530

    test: add two-number comma-separated test (red)

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,14 +2,18 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final calc = StringCalculator();
+
   test('empty string returns 0', () {
-    final calc = StringCalculator();
     expect(calc.add(''), equals(0));
   });
 
   test('single number returns itself', () {
-    final calc = StringCalculator();
     expect(calc.add('1'), equals(1));
     expect(calc.add('42'), equals(42));
+  });
+
+  test('two numbers, comma separated', (){
+    expect(calc.add('1,5'), equals(6));
   });
 }
